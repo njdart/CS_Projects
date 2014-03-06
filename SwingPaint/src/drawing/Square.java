2f504bf1947292414a695f438e113a59eb572723
Nic <nicholasjdart@gmail.com>
2014-03-06 01:07:54 +0000

    Actually added the square

---
+++ SwingPaint/src/drawing/Square.java
@@ -0,0 +1,29 @@
+package drawing;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Square extends PaintObject {
+
+	public Square(int x, int y, int size, Color color) {
+		super(x, y, size, color);
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		g.setColor(color);
+		g.fillRect((int)Math.round(this.x - size/2), (int)Math.round(this.y - size/2), size, size);
+	}
+
+	@Override
+	public void remove(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect((int)Math.round(this.x - size/2), (int)Math.round(this.y - size/2), size, size);
+	}
+
+	@Override
+	public String getType() {
+		return "Square";
+	}
+
+}
