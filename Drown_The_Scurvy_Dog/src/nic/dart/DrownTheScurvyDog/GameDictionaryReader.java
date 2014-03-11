069b3bc774efe1425e25e33aab2901a94cf47944
Nic <nicholasjdart@gmail.com>
2014-03-11 21:03:44 +0000

    Implemented the writeDictionary method

--- Drown_The_Scurvy_Dog/src/nic/dart/DrownTheScurvyDog/GameDictionaryReader.java
+++ Drown_The_Scurvy_Dog/src/nic/dart/DrownTheScurvyDog/GameDictionaryReader.java
@@ -1,10 +1,14 @@
 package nic.dart.DrownTheScurvyDog;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import com.google.gson.Gson;
 
 public class GameDictionaryReader {
@@ -19,13 +23,19 @@ public class GameDictionaryReader {
 	}
 	
 	public PhraseBook readDictionary() throws FileNotFoundException{
-		//BufferedWriter s = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("students.json"))
-		
 		try(BufferedReader dictReader = new BufferedReader(new InputStreamReader(new FileInputStream(dict)))){
 			return g.fromJson(dictReader, PhraseBook.class);
 		} catch (IOException e){
 			e.printStackTrace();
 			return null;
+		}
+	}
+	
+	public void writeDictionary(PhraseBook pb) throws FileNotFoundException {
+		try (BufferedWriter dictWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dict)))){
+			g.toJson(pb, dictWriter);
+		} catch (IOException e){
+			e.printStackTrace();
 		}
 	}
 }
